4ac29e87487589cdf35e4c8315f804afff14f88c
DanielClemKim <sukimclem@gmail.com>
2025-07-01 23:29:47 +0900

    delete nuxt

--- blog-frontend/pages/posts/edit/[id].vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container mx-auto py-8">
-    <NuxtLink to="/" class="text-blue-500 hover:underline">← 목록으로</NuxtLink>
-    <h1 class="text-2xl font-bold mb-6 mt-4">글 수정</h1>
-    <PostForm :editMode="true" />
-  </div>
-</template>
-
-<script setup lang="ts"></script>
